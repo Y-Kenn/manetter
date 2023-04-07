b04ddd04e5354503a0f2ec681b695dfa07fb83a4
Y-Kenn <125204607+Y-Kenn@users.noreply.github.com>
2023-04-07 15:45:55 +0900

    webフォント追加によりブラウザ間のフォントの統一

--- head.php
+++ head.php
@@ -10,4 +10,7 @@
         <script src="https://kit.fontawesome.com/1329293142.js" crossorigin="anonymous"></script>
         <script src="https://www.gstatic.com/charts/loader.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
     </head>
